2d1298bb65ac50d2361d8db8c2db02a29f531147
Nam Gyumo <mjc5433@gmail.com>
2025-08-26 19:35:12 +0900

    make map and monster spown ver

--- RPG project in vacation/RPG project.cpp
+++ RPG project in vacation/RPG project.cpp
@@ -7,6 +7,15 @@
 #include<ctime>
 #include<memory>
 
+
+int random(int min, int max) {
+	static std::random_device rd;
+	static std::mt19937 gen(rd());
+
+	std::uniform_int_distribution<> dist(min, max);
+	return dist(gen);
+}
+	
 class Living {
 protected:
 	std::string name;
@@ -89,7 +98,7 @@ public:
 		case CharacterType::Archer: damage_init = 12; hp_init = 18; defence_init = 10; break;
 		}
 		characters.push_back(std::make_unique<Character>(
-			std::move(name), age, damage_init, hp_init, defence_init,lv,live));
+			std::move(name), age, damage_init, hp_init, defence_init,lv,live,character_type));
 		return characters.back().get();
 	}
 	const std::vector<std::unique_ptr<Character>>& all() const noexcept { return characters; }
@@ -105,9 +114,9 @@ public:
 class Monster_Manager {
 	std::vector<std::unique_ptr<Monster>> monsters;
 public:
-	Monster* make_monster(const std::string& name, int age,int damage_init,int hp_init,int defence_init,int lv ) {
+	Monster* make_monster(const std::string& name, int age,int damage_init,int hp_init,int defence_init,int lv,MonsterType monster_type) {
 		bool live = true;
-		monsters.push_back(std::make_unique<Monster>(std::move(name), age, damage_init, hp_init, defence_init, lv,live));
+		monsters.push_back(std::make_unique<Monster>(std::move(name), age, damage_init, hp_init, defence_init, lv,live,monster_type));
 		return monsters.back().get();
 	}
 	std::vector<std::unique_ptr<Monster>>& all() { return monsters; }
@@ -119,6 +128,44 @@ public:
 			}
 		}
 	}
+
+	std::string to_string(MonsterType type) {
+		switch (type) {
+		case MonsterType::Undead:   return "Undead";
+		case MonsterType::Humanoid: return "Humanoid";
+		case MonsterType::Animal:   return "Animal";
+		case MonsterType::Dragon:   return "Dragon";
+		}
+	}
+
+	Monster* make_random_monster(int lv) {
+
+		std::string name_data[21] = { "Angry", "Brutal", "Cunning", "Dark", "Elder",
+		"Fierce", "Giant", "Hungry", "Infernal", "Jeweled",
+		"Keen", "Lethal", "Mighty", "Nimble", "Obsidian",
+		"Poisonous", "Quick", "Ruthless", "Savage", "Terrifying" };
+
+		int typenum = random(1, 100);
+		if (typenum > 41) { typenum = 1; }
+		else if (typenum > 61) { typenum = 2; }
+		else if (typenum > 91) { typenum = 3; }
+		else { typenum = 4; }
+		MonsterType monster_type = static_cast<MonsterType>(typenum);
+		int damage_init, hp_init, defence_init;
+		switch (monster_type) {
+		case MonsterType::Undead: damage_init = 8; hp_init = 15; defence_init = 4; break;
+		case MonsterType::Humanoid: damage_init = 15; hp_init = 20; defence_init = 8; break;
+		case MonsterType::Animal: damage_init = 6; hp_init = 15; defence_init = 13; break;
+		case MonsterType::Dragon: damage_init = 20; hp_init = 25; defence_init = 3; break;
+		}
+		damage_init *= 1 + (lv * 0.5);
+		hp_init *= 1 + (lv * 0.5);
+		defence_init *= 1 + (lv * 0.5);
+		std::string name = name_data[random(0, 19)] +" "+ to_string(monster_type);
+		int age = random(1, 500);
+		Monster* monster = make_monster(name, age, damage_init, hp_init, defence_init, lv, monster_type);
+		return monster;
+	}
 	
 };
 
@@ -126,22 +173,27 @@ class Map {
 public:
 	struct Map_node {
 		int x, y;
-		bool visited{false};
-		bool char_here{false};
+		bool visited{ false };
+		bool char_here{ false };
 		struct Map_node* front{ nullptr };
 		struct Map_node* back{ nullptr };
 		struct Map_node* left{ nullptr };
 		struct Map_node* right{ nullptr };
-		Monster* monster{nullptr};
+		Monster* monster{ nullptr };
 	};
-	Map_node* map_creation(int x,int y) {
+private:
+	Monster_Manager monster_manager;
+	std::vector<std::vector<Map_node*>> grid;
+public:
+	Map_node* map_creation(int x,int y,Monster* monster) {
 		Map_node* new_map = new Map_node;
 		new_map->x = x;
 		new_map->y = y;
+		new_map->monster = monster;
 		return new_map;
 	}
 	void make_map(int max_x, int max_y) {
-		std::vector<std::vector<Map_node*>> grid;
+		grid.clear();
 		grid.resize(max_y);
 		for (int y = 0; y < max_y; y++) {
 			grid[y] = std::vector<Map_node*>(max_x, nullptr);
@@ -149,7 +201,9 @@ public:
 
 		for (int y = 0; y < max_y; y++) {
 			for (int x = 0; x < max_x; x++) {
-				grid[y][x] = map_creation(x, y);
+				int monster_lv = x > y ? x : y;
+				Monster* monster = monster_manager.make_random_monster(monster_lv);
+				grid[y][x] = map_creation(x, y,monster);
 			}
 		}
 
